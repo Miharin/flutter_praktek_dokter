09059feb88d328b53529909a351681f9ec53aa3f
Miharin <asunayuuki289@gmail.com>
2024-01-04 16:52:05 +0700

    model

---
+++ lib/models/user/authentication_model.dart
@@ -0,0 +1,10 @@
+class AuthenticationModel {
+  AuthenticationModel({
+    required this.username,
+    required this.password,
+    required this.token,
+  });
+  final String username;
+  final String password;
+  final String token;
+}
