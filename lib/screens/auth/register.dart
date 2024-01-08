ea8b090bade22f8d5a99fc16264a6dc562652aac
Vanilla-Nya <fontaro990@gmail.com>
2024-01-08 22:39:18 +0700

    .

--- lib/screens/auth/register.dart
+++ lib/screens/auth/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_praktek_dokter/helpers/auth/register_helper.dart';
 import 'package:flutter_praktek_dokter/widget/custom_textfromfield/custom_textformfield.dart';
+import 'package:gap/gap.dart';
 import 'package:get/get.dart';
 
 class RegisterScreen extends StatelessWidget {
@@ -36,15 +37,11 @@ class RegisterScreen extends StatelessWidget {
                 children: [
                   SizedBox(
                     child: Padding(
-                      padding: const EdgeInsets.only(
-                        top: 6.0,
-                        left: 6.0,
-                        right: 6.0,
-                        bottom: 6.0,
-                      ),
+                      padding: const EdgeInsets.only(),
                       child: ExpansionTile(
-                        shape:
-                            const RoundedRectangleBorder(side: BorderSide.none),
+                        shape: const RoundedRectangleBorder(
+                          side: BorderSide.none,
+                        ),
                         title: Center(
                           child: Text(
                             "Autentikasi".toUpperCase(),
@@ -88,9 +85,11 @@ class RegisterScreen extends StatelessWidget {
                     shape: const RoundedRectangleBorder(side: BorderSide.none),
                     title: Center(
                         child: Text(
-                      "PROFILE".toUpperCase(),
+                      "Bio Data".toUpperCase(),
                       style: const TextStyle(
-                          fontSize: 16.0, fontWeight: FontWeight.bold),
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     )),
                     children: [
                       const Divider(
@@ -113,8 +112,9 @@ class RegisterScreen extends StatelessWidget {
                         ),
                       ),
                       ExpansionTile(
-                        shape:
-                            const RoundedRectangleBorder(side: BorderSide.none),
+                        shape: const RoundedRectangleBorder(
+                          side: BorderSide.none,
+                        ),
                         title: Center(
                             child: Text(
                           "Alamat".toUpperCase(),
@@ -149,6 +149,49 @@ class RegisterScreen extends StatelessWidget {
                       )
                     ],
                   ),
+                  ExpansionTile(
+                    shape: const RoundedRectangleBorder(
+                      side: BorderSide.none,
+                    ),
+                    title: Center(
+                      child: Text(
+                        "Bio Data Lengkap".toUpperCase(),
+                        style: const TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    children: [
+                      const Divider(
+                        indent: 20.0,
+                        endIndent: 20.0,
+                        color: Colors.black38,
+                      ),
+                      Obx(
+                        () => CustomTextFromField(
+                          listLabel: const [
+                            "Email",
+                            "Password",
+                            "NIK",
+                            "Nama",
+                          ],
+                          type: const [
+                            TextInputType.emailAddress,
+                            TextInputType.text,
+                            TextInputType.number,
+                            TextInputType.name
+                          ],
+                          lengthList: 4,
+                          constraints: constrainedV2,
+                          verification:
+                              _registerHelper.listVerification.isEmpty,
+                          errorText: 'Wajib Di Isi',
+                          listController: controller,
+                        ),
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
@@ -156,19 +199,5 @@ class RegisterScreen extends StatelessWidget {
         );
       },
     );
-  }
-}
-
-class CalenderPicker extends GetxController {
-  TextEditingController dateinput = TextEditingController();
-
-  calenderPicker(formattedDate) {
-    dateinput.text = formattedDate;
-  }
-
-  @override
-  void dispose() {
-    dateinput.dispose();
-    super.dispose();
   }
 }
